0198bb955b77a18b6f4864e04a923f5c0bff9aa4
Vennethe Valenzuela <vennethev@unr.edu>
2024-12-03 22:24:32 +0000

    First commit!

---
+++ madlib.c
@@ -0,0 +1,107 @@
+// Authors: James Dye & Vennethe Y. Valenzuela 
+// Date: 12/02/20
+// Purpose: To create a Madlib game that a user can play within the terminal.
+
+#include <stdio.h>
+
+// MACROS
+#define MAX_STRING_SIZE 1000
+
+// FILES
+#define INPUT_FILE_1 madlib1.txt
+#define INPUT_FILE_2 madlib2.txt
+
+// FUNCTION PROTOTYPES BELOW
+int getStringLength(char str[]); 
+
+void requestUserInput(int size, char madlibChars[], char madlibResponses[][MAX_STRING_SIZE]);
+void removeEndlineChar (int size, char string[]);
+void removeEndlineChar2 (char str[]);
+
+// MAIN FUNCTION BELOW
+int main(){	
+	FILE inputFile = fopen(INPUT_FILE_1, "r"); // Open a madlib.txt file
+	
+	// Verify connection is made to a madlib.txt file
+	if (inputFile == NULL){
+		printf("Could not open a file for reading!\n");
+		return 0;
+	}
+	
+	fclose(inputFile); // Close the madlib.txt file 
+	
+	return 0;
+	
+}// END of main
+
+void storeText(FILE* fileToRead, int rows, int cols, char str[][cols]){
+	for (int i = 0; i < rows; i++){
+		fgets(str[i], cols, fileToRead); // At the "i"th row of str, of size cols, appends from fileToRead
+	}
+}
+
+int getNumLinesFromFile(FILE* fileToRead, int maxColSize){
+	int counter = 0;
+	char temp[maxColSize];
+	while (fgets(temp, maxColSize, fileToRead) != NULL){
+		counter++;
+	}
+	return counter;
+}
+
+// FUNCTION DEFINITIONS BELOW
+int getStringLength(char str[]){
+// PURPOSE: gets the length of a string using a for loop to count through how many indexes it takes until a null character ('\0') is reached.
+	int counter = 0;
+	while (str[counter] != '\0'){
+		counter++;
+	}
+	return counter;
+	
+}// END of getStringLength
+
+
+
+
+/*
+// PURPOSE: Creates a char var holder and interates through a char array to display to the screen requests and waits for a response before reasking.	
+void requestUserInput(int size, char madlibChars[]){
+	char holder = madlibChars[0];
+	char response[MAX_STRING_SIZE];
+	
+	for (int i = 0; i < size; i++){
+		holder = madlibChars[i];
+		if (holder == 'A'){
+			printf("Please enter an adjective: ");	
+		} else {
+			printf("Please enter a ");
+			if (holder == 'N'){
+				printf("noun: ");
+			} else if (holder == 'V'){
+				printf("verb: ");
+			} // End of nested if/else if statement
+		} // End of if/else statement
+		
+		fgets(responses, size, stdin);
+		
+	} // End of for loop	
+
+}// END of requestUserInput
+
+void removeEndlineChar (int size, char string[]){
+// PURPOSE: Interates through a string and once a null character is detected, makes the character a space (' ')	
+	for (int i = 0; i < size; i++){
+		if (string[i] == '\0'){
+			string[i] = ' ';
+		}
+	}
+	
+}// END of removeEndLineChar
+
+// Another way to write removeEndlineChar
+void removeEndlineChar2 (char str[]){
+	int endlineIndex = getStringLength(str); 
+	str[endlineIndex] = ' '; //Sets the endline character of the string as a space 
+	
+} // END of removeEndLineChar2
+*/
